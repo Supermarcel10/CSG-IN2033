25f5b300447d8b28be9fd1ecb9acd24b3a3717ce
Supermarcel10 <marcelbarlik@gmail.com>
2024-04-14 03:32:19 +0100

    - Added version label that uses artifact version from maven. - Rewritten Main by splitting each section into its own method.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,9 @@
 module IN2033 {
 	requires javafx.controls;
 	requires javafx.fxml;
-
 	requires com.zaxxer.hikari;
 	requires java.sql;
+	requires org.dom4j;
 
 	opens uk.ac.city to javafx.fxml;
 	exports uk.ac.city;
